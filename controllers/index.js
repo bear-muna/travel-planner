2f62b63963f01e013cc402e925c2562a09813d28
bear-muna <bearmuna@gmail.com>
2023-05-03 12:42:41 -0700

    adding location controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const travellerRoutes = require('./travellerController');
+const locationRoutes = require('./locationController');
 
 router.get('/', (req, res) => {
     res.send("WELCOME TO THE HOMEPAGE");
 });
 
 router.use('/api/travellers', travellerRoutes);
+router.use('/api/locations', locationRoutes)
 
 module.exports = router;
